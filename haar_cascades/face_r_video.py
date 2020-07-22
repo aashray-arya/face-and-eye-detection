e846489a7c1dcd1951358657e27c64bdd0698ddc
aashray-arya <59815435+aashray-arya@users.noreply.github.com>
2020-07-23 01:03:11 +0530

    Update face_r_video.py

--- haar_cascades/face_r_video.py
+++ haar_cascades/face_r_video.py
@@ -4,21 +4,21 @@ import numpy as np
 import cv2
 import time
 
-face_cascade = cv2.CascadeClassifier('/home/user/haar_cascades/frontalface.xml')
-eyes_cascade = cv2.CascadeClassifier('/home/user/haar_cascades/eye.xml')
-        
+face_cascade = cv2.CascadeClassifier('/home/aashray/catkin_ws/src/ros_live/haar_cascades/frontalface.xml')
+eyes_cascade = cv2.CascadeClassifier('/home/aashray/catkin_ws/src/ros_live/haar_cascades/eye.xml')
+roi=None
+
 def detect_face_and_eyes(image_frame):
 
 	img_original=image_frame
 	
-	img=image_frame    
+	img=image_frame
 
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
 	ScaleFactor = 1.2
 
-	minNeighbors = 1
+	minNeighbors = 3
 
 	face = face_cascade.detectMultiScale(gray, ScaleFactor, minNeighbors)
 
@@ -27,17 +27,18 @@ def detect_face_and_eyes(image_frame):
 		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)  
 		roi = img[y:y+h, x:x+w]
 
-	 eyes = eyes_cascade.detectMultiScale(roi)
-	
-	 for (ex,ey,ew,eh) in eyes:
-	 	cv2.rectangle(roi, (ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	if(roi is not None):
+		eyes = eyes_cascade.detectMultiScale(roi)
+
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(roi, (ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 	cv2.imshow('Face_original',img_original)
 	cv2.imshow('Face',img)
 
 def main():
     #video_capture = cv2.VideoCapture(0)  #for live tracking
-    video_capture = cv2.VideoCapture('video_path') #for tracking in a recorded video
+    video_capture = cv2.VideoCapture('/home/aashray/Downloads/Bday_many.mp4') #for tracking in a recorded video
 
     while(True):
         ret, frame = video_capture.read()
