de239daf157c3c0b1d7da6ddeed6c0ee0d5f11b9
aashray-arya <59815435+aashray-arya@users.noreply.github.com>
2020-07-23 01:05:11 +0530

    Update face_r.py

--- haar_cascades/face_r.py
+++ haar_cascades/face_r.py
@@ -6,8 +6,8 @@ import cv2
 face_cascade = cv2.CascadeClassifier('/home/user/haar_cascades/frontalface.xml')
 eyes_cascade = cv2.CascadeClassifier('/home/user/haar_cascades/eye.xml')
         
-img_original = cv2.imread('/home/user/img.jpg')#Enter the location of the photo
-  
+img_original = cv2.imread('image_path')#add the path to your image
+roi = None 
 
 img_original = cv2.resize(img_original,(500,300))
 
@@ -26,9 +26,12 @@ for (x,y,w,h) in face:
 
 	cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)  
 	roi = img[y:y+h, x:x+w]
-eyes = eyes_cascade.detectMultiScale(roi)
-for (ex,ey,ew,eh) in eyes:
- 	cv2.rectangle(roi, (ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+if(roi is not None):
+
+	eyes = eyes_cascade.detectMultiScale(roi)
+
+	for (ex,ey,ew,eh) in eyes:
+		cv2.rectangle(roi, (ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 
 cv2.imshow('Face_original',img_original)
@@ -36,7 +39,3 @@ cv2.imshow('Face_original',img_original)
 cv2.imshow('Face',img)
 
 cv2.waitKey(1)
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
